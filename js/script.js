461207b25c076b1be1266e5887c5f8522e4b4eca
Adam Bogocz <adi_bog@o2.pl>
2023-02-10 14:41:00 +0100

    add background and icon

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 console.log("Cześć!");
 
-let cashElement = document.querySelector(".js-cash");
-let exchangeRateElement = document.querySelector(".js-exchangeRate");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
+{
+    let cashElement = document.querySelector(".js-cash");
+    let exchangeRateElement = document.querySelector(".js-exchangeRate");
+    let formElement = document.querySelector(".js-form");
+    let resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("input", () => {
-    let cash = cashElement.value;
-    let exchangeRate = exchangeRateElement.value;
-    let result = cash * exchangeRate
-    resultElement.innerText = `${cash} PLN = ${result.toFixed(2)} Euro`;
-})
+    formElement.addEventListener("input", () => {
+        let cash = cashElement.value;
+        let exchangeRate = exchangeRateElement.value;
+        let result = cash * exchangeRate;
+        resultElement.innerText = `${cash} PLN = ${result.toFixed(2)} Euro`;
+    })
+}
