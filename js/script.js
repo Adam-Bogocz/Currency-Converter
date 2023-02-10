cc4f2564f2f59b0cffa531d53341c5696bf26598
Adam Bogocz <adi_bog@o2.pl>
2023-02-10 23:39:10 +0100

    add function and input type="radio"

--- js/script.js
+++ js/script.js
@@ -1,15 +1,46 @@
 console.log("Cześć!");
 
 {
-    let cashElement = document.querySelector(".js-cash");
-    let exchangeRateElement = document.querySelector(".js-exchangeRate");
-    let formElement = document.querySelector(".js-form");
-    let resultElement = document.querySelector(".js-result");
 
-    formElement.addEventListener("input", () => {
-        let cash = cashElement.value;
-        let exchangeRate = exchangeRateElement.value;
-        let result = cash * exchangeRate;
+
+    //   const formCurrencyElement = document.querySelector(".js-currencyEUR");
+    //         const currency = formCurrencyElement.value;
+
+
+    // const formDirectionElement = document.querySelector(".js-directionPLN-currency")
+
+    const calculateResult = (cash, exchangeRate) => {
+
+        // console.log(formCurrencyElement.checked); console.log(currency);
+
+        // if formCurrencyElement.checked {
+        //     currency = "EUR";
+        //     console.log(currency);
+        // }
+        result = (cash * exchangeRate);
+
+    }
+
+    const updateResultText = (cash, result) => {
+        const resultElement = document.querySelector(".js-result");
         resultElement.innerText = `${cash} PLN = ${result.toFixed(2)} Euro`;
-    })
+    }
+
+    const onFormSubmit = () => {
+        const cashElement = document.querySelector(".js-cash");
+        const exchangeRateElement = document.querySelector(".js-exchangeRate");
+
+        const cash = cashElement.value;
+        const exchangeRate = exchangeRateElement.value;
+
+        calculateResult(cash, exchangeRate);
+        updateResultText(cash, result);
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("input", onFormSubmit);
+    }
+
+    init();
 }
