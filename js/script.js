92215a86fa04d4ac638320865a7a53caed702987
Adam Bogocz <adi_bog@o2.pl>
2023-02-12 12:11:49 +0100

    update currency TEXT

--- js/script.js
+++ js/script.js
@@ -1,46 +1,94 @@
 console.log("Cześć!");
 
 {
-
-
-    //   const formCurrencyElement = document.querySelector(".js-currencyEUR");
-    //         const currency = formCurrencyElement.value;
-
-
-    // const formDirectionElement = document.querySelector(".js-directionPLN-currency")
-
-    const calculateResult = (cash, exchangeRate) => {
-
-        // console.log(formCurrencyElement.checked); console.log(currency);
-
-        // if formCurrencyElement.checked {
-        //     currency = "EUR";
-        //     console.log(currency);
-        // }
-        result = (cash * exchangeRate);
-
+    const calculateResult = (cash, exchangeRate, direction) => {
+        switch (direction) {
+            case "fromPLN":
+                return result = (cash * exchangeRate);
+            case "toPLN":
+                return result = (cash / exchangeRate);
+        }
     }
 
-    const updateResultText = (cash, result) => {
+    const updateResultText = (cash, result, direction, currency) => {
         const resultElement = document.querySelector(".js-result");
-        resultElement.innerText = `${cash} PLN = ${result.toFixed(2)} Euro`;
+        switch (direction) {
+            case "fromPLN":
+                if (currency === "EUR") {
+                    return resultElement.innerText = `${cash} PLN = ${result.toFixed(2)} EUR`;
+                }
+                if (currency === "USD") {
+                    return resultElement.innerText = `${cash} PLN = ${result.toFixed(2)} USD`;
+                }
+            case "toPLN":
+                if (currency === "EUR") {
+                    return resultElement.innerText = `${cash} EUR = ${result.toFixed(2)} PLN`;
+                }
+                if (currency === "USD") {
+                    return resultElement.innerText = `${cash} USD = ${result.toFixed(2)} PLN`;
+                }
+        }
+    }
+
+    const updateCurrencyText = (currency) => {
+        const formDirectionFromPlnTextElement = document.querySelector(".js-directionFromPlnText");
+        const formDirectionToPlnTextElement = document.querySelector(".js-directionToPlnText");
+        const exchangeRateTextElement = document.querySelector(".js-exchangeRateText");
+        const cashTextElement = document.querySelector(".js-cashText");
+        switch (currency) {
+            case "EUR":
+                formDirectionFromPlnTextElement.innerText = `PLN => EUR`;
+                formDirectionToPlnTextElement.innerText = `EUR => PLN`;
+                exchangeRateTextElement.innerText = `kurs EUR:`;
+                if (direction === "fromPLN") {
+                    cashTextElement.innerText = `Podaj kwotę PLN`;
+                } else {
+                    cashTextElement.innerText = `Podaj kwotę EUR`;
+                }
+                return;
+            case "USD":
+                formDirectionFromPlnTextElement.innerText = `PLN => USD`;
+                formDirectionToPlnTextElement.innerText = `USD => PLN`;
+                exchangeRateTextElement.innerText = `kurs USD:`;
+                if (direction === "toPLN") {
+                    cashTextElement.innerText = `Podaj kwotę USD`;
+                } else {
+                    cashTextElement.innerText = `Podaj kwotę PLN`;
+                }
+                return;
+        }
     }
 
     const onFormSubmit = () => {
         const cashElement = document.querySelector(".js-cash");
         const exchangeRateElement = document.querySelector(".js-exchangeRate");
+        const formCurrencyElement = document.querySelector(".js-currencyEUR");
+        const formDirectionFromPlnElement = document.querySelector(".js-directionFromPLN");
 
         const cash = cashElement.value;
         const exchangeRate = exchangeRateElement.value;
+      
+        if (formCurrencyElement.checked === true) {
+            currency = "EUR";
+        } else {
+            currency = "USD";
+        }
 
-        calculateResult(cash, exchangeRate);
-        updateResultText(cash, result);
+        if (formDirectionFromPlnElement.checked === true) {
+            direction = "fromPLN";
+        } else {
+            direction = "toPLN";
+        }
+
+        updateCurrencyText(currency);
+        calculateResult(cash, exchangeRate, direction);
+        updateResultText(cash, result, direction, currency);
     }
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("input", onFormSubmit);
     }
-
+   
     init();
 }
